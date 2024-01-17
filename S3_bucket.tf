20ecda75cc8e6d4df87d2b97f733417b2622a185
medorock25 <56940194+medorock25@users.noreply.github.com>
2024-01-17 11:33:23 +0100

    Update S3_bucket.tf
    Adding Object  lambda access point

--- S3_bucket.tf
+++ S3_bucket.tf
@@ -1,3 +1,4 @@
+## S3 Bucket
 resource "aws_s3_bucket" "s3_test" {
   bucket = "my-tf-test-bucket"
 
@@ -7,8 +8,28 @@ resource "aws_s3_bucket" "s3_test" {
   }
 }
 
+## Creating access Point
+
 resource "aws_s3_access_point" "access_point_test" {
   bucket = aws_s3_bucket.s3_test.id
   name   = "access_point"
 }
 
+## Object lambda access Point
+resource "aws_s3control_object_lambda_access_point" "example" {
+  name = "example"
+
+  configuration {
+    supporting_access_point = aws_s3_access_point.access_point_test.arn
+
+    transformation_configuration {
+      actions = ["GetObject"]
+
+      content_transformation {
+        aws_lambda {
+          function_arn = aws_lambda_function.example.arn
+        }
+      }
+    }
+  }
+}
